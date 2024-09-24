848331bfe04121a11d09b9e13dd0e2d61c4713df
AlAsh88 <shk.ayesha@hotmail.com>
2024-09-23 21:27:21 -0400

    Initial commit

---
+++ Weather-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather-App
+//
+//  Created by Ayesha Shaikh on 9/23/24.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather-AppTests/Weather_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppTests.swift
+//  Weather-AppTests
+//
+//  Created by Ayesha Shaikh on 9/23/24.
+//
+
+import Testing
+@testable import Weather_App
+
+struct Weather_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
